7b838323399787d88cc9c514f9bfef1ad865d738
shivamisrani <shivam.israni12@gmail.com>
2024-10-04 20:53:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+
+
+
